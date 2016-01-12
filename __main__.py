0a30840650a9253e35b8c9368ef823810d72bbba
pmdcosta <pmdcosta@outlook.com>
2016-01-12 23:10:19 +0000

    Protected agains evil exceptions

--- __main__.py
+++ __main__.py
@@ -1,12 +1,43 @@
 #!/usr/bin/env python
 
-from classes.options_test import Options
+import options
+import help
 import sys
 
-ops = ['help', 'bot_list', 'task_submit', 'plugin_install', 'task_status', 'task_clear', 'task_output', 'task_list',
-       'plugin_list', 'plugin_clear', 'plugin_help']
-if len(sys.argv) < 2 or sys.argv[1] not in ops:
-    print('Invalid command.\nUse help for a list of commands.')
-else:
-    getattr(Options, sys.argv[1])(sys.argv[2:])
+ops = {
+    'knight_ls':        ['get', 'knight/list'],
+    'task_run':         ['post', 'task'],
+    'task_ls':          ['get', 'task/list'],
+    'task_check':       ['get', 'task/{param}/check'],
+    'task_output':      ['get', 'task/{param}/output'],
+    'task_rm':          ['delete', 'task/{param}'],
+    'plugin_install':   ['post', 'plugin'],
+    'plugin_ls':        ['get', 'plugin/list'],
+    'plugin_rm':        ['delete', 'plugin/{param}'],
+    'plugin_help':      ['get', 'plugin/{param}/help']
+}
 
+
+if len(sys.argv) < 4:
+    if len(sys.argv) > 2 and sys.argv[2] in ops.keys():
+        getattr(help, sys.argv[2])
+    else:
+        help.help()
+else:
+    if sys.argv[3] in ops.keys():
+        context = {
+            'host': sys.argv[1],
+            'port': sys.argv[2],
+            'command': sys.argv[3],
+            'method': ops[sys.argv[3]][0],
+            'endpoint': ops[sys.argv[3]][1],
+            'args': sys.argv[4:],
+            'json': None,
+            'data': '',
+            'param': '',
+            'headers': ''
+        }
+        print(getattr(options, sys.argv[3])(context))
+    else:
+        print("Invalid Command\n")
+        help.help()
